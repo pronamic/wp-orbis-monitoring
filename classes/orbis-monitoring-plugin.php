baaf5eb0f56c53f4373fe0221d71cd109d422d42
I <info@remcotolsma.nl>
2016-09-28 17:31:57 +0200

    Added a extra Slack event.

--- classes/orbis-monitoring-plugin.php
+++ classes/orbis-monitoring-plugin.php
@@ -284,6 +284,8 @@ class Orbis_Monitoring_Plugin extends Orbis_Plugin {
 		if ( '200' !== $response_code ) {
 			do_action( 'orbis_monitor_problem', $post );
 		}
+
+		do_action( 'orbis_monitor_checked', $post );
 	}
 
 	public function monitor() {
@@ -337,6 +339,20 @@ class Orbis_Monitoring_Plugin extends Orbis_Plugin {
 		$events['orbis_monitor_problem'] = array(
 			'action'      => 'orbis_monitor_problem',
 			'description' => __( 'When a Orbis monitor problem was detected.', 'orbis_monitoring' ),
+			'message'     => function( $post ) {
+				return sprintf(
+					__( 'Orbis monitor <%s|%s> was just checked, response code was `%s` » %s.', 'orbis_monitoring' ),
+					get_permalink( $post ),
+					get_the_title( $post ),
+					get_post_meta( $post->ID, '_orbis_monitor_response_code', true ),
+					get_post_meta( $post->ID, '_orbis_monitor_url', true )
+				);
+			},
+		);
+
+		$events['orbis_monitor_checked'] = array(
+			'action'      => 'orbis_monitor_checked',
+			'description' => __( 'When a Orbis monitor was checked.', 'orbis_monitoring' ),
 			'message'     => function( $post ) {
 				return sprintf(
 					__( 'Orbis monitor <%s|%s> was just checked, response code was `%s` » %s.', 'orbis_monitoring' ),
